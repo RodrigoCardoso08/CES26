756f07ed99d065b2b8db31f2029d0a70d2acaf4d
rodrigocardosoo15 <rodrigo.cardoso@arcca.io>
2023-08-29 12:43:33 -0300

    created initial elements

---
+++ serie2/js/game.js
@@ -0,0 +1,95 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+let soundEnabled = true;
+let planeImg = new Image();
+planeImg.onload = function() {};
+planeImg.src = 'file:///C:/Users/Rodri/Documents/ITA/4%C2%BA%20semestre/CES26/CES26/serie2/assets/image/aviao.png';
+
+
+let missileImg = new Image();
+missileImg.onload = function() {};
+missileImg.src = 'file:///C:/Users/Rodri/Documents/ITA/4%C2%BA%20semestre/CES26/CES26/serie2/assets/image/missil.png';
+
+let plane = {
+  x: 0,
+  y: 0,
+  width: 50,
+  height: 50
+};
+
+let missile = {
+  x: 400,
+  y: 300,
+  width: 20,
+  height: 20,
+  speed: 5,
+  fired: false
+};
+
+function toggleSound() {
+  soundEnabled = !soundEnabled;
+}
+
+function playSound(id) {
+    console.log("on playSound, id = ", id);
+//   if (soundEnabled) {
+//     const audio = document.getElementById(id);
+//     audio.play();
+//   }
+}
+
+function drawPlane() {
+    console.log("planeImg = ", planeImg);
+    if (planeImg.complete) {
+      ctx.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height);
+    }
+  }
+  
+  function drawMissile() {
+    ctx.drawImage(missileImg, missile.x, missile.y, missile.width, missile.height);
+  }
+
+function updateMissile() {
+  if (missile.fired) {
+    const dx = plane.x - missile.x;
+    const dy = plane.y - missile.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    missile.x += (dx / distance) * missile.speed;
+    missile.y += (dy / distance) * missile.speed;
+
+    if (
+      missile.x < plane.x + plane.width &&
+      missile.x + missile.width > plane.x &&
+      missile.y < plane.y + plane.height &&
+      missile.y + missile.height > plane.y
+    ) {
+      playSound("explosionSound");
+      missile.fired = false;
+      missile.x = 400;
+      missile.y = 300;
+    }
+  }
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPlane();
+  drawMissile();
+  updateMissile();
+  requestAnimationFrame(gameLoop);
+}
+
+canvas.addEventListener("mousemove", function (event) {
+  const rect = canvas.getBoundingClientRect();
+  plane.x = event.clientX - rect.left;
+  plane.y = event.clientY - rect.top;
+});
+
+canvas.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+  missile.fired = true;
+  playSound("missileSound");
+});
+
+gameLoop();
